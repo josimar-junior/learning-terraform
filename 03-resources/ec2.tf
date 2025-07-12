e42b591fe1b1b3fb34d97d9878f4bc521ab3b906
Josimar Junior <josimarjunior@Josimars-MacBook-Pro.local>
2025-07-12 18:41:49 -0300

    creating resources

---
+++ 03-resources/ec2.tf
@@ -0,0 +1,43 @@
+resource "aws_instance" "web" {
+  # AMI ID Amazon Ubuntu = ami-05ffe3c48a9991133
+  # AMI ID NGINX         = ami-08b00e6f894a62af3
+  ami                         = "ami-08b00e6f894a62af3"
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.public_http_traffic.id]
+
+  tags = merge(local.default_tags, {
+    Name = "03-resources-ec2"
+  })
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "public_http_traffic" {
+  description = "Security group allowing traffic on ports 443 and 80"
+  name        = "public-http-traffic"
+  vpc_id      = aws_vpc.main.id
+
+  tags = merge(local.default_tags, {
+    Name = "03-resources-sg"
+  })
+}
+
+resource "aws_vpc_security_group_ingress_rule" "http" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.public_http_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+}
