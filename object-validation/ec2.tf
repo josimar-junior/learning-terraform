163cd40d2da8ef32344729d6fcbff4d681c1fe08
Josimar Junior <josimarjunior@Josimars-MacBook-Pro.local>
2025-08-15 21:56:19 -0300

    working with check blocks

--- object-validation/ec2.tf
+++ object-validation/ec2.tf
@@ -21,10 +21,21 @@ resource "aws_instance" "main" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
 
+  tags = {
+    CostCenter = "12345"
+  }
+
   lifecycle {
     precondition {
       condition     = contains(local.allowed_instance_types, var.instance_type)
       error_message = "Invalid instance type"
     }
+  }
+}
+
+check "cost_center_check" {
+  assert {
+    condition     = can(aws_instance.main.tags.CostCenter != "")
+    error_message = "Your AWS instance does not have a CostCenter tag."
   }
 }
--- object-validation/variables.tf
+++ object-validation/variables.tf
@@ -1,4 +1,4 @@
 variable "instance_type" {
   type    = string
-  default = "t2.medium"
+  default = "t2.micro"
 }
