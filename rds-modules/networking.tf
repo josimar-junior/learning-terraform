33ca10d1917dd628144f76e36a5fdc9cc7f0767e
Josimar Junior <josimarjunior@Josimars-MacBook-Pro.local>
2025-08-31 13:26:29 -0300

    testing sg rule validation

--- rds-modules/networking.tf
+++ rds-modules/networking.tf
@@ -52,3 +52,37 @@ resource "aws_subnet" "not_allowed" {
     Name = "subnet-not-allowed"
   }
 }
+
+resource "aws_security_group" "source" {
+  name        = "source-sg"
+  description = "SG from where connections are allowed into the DB"
+  vpc_id      = aws_vpc.custom.id
+}
+
+resource "aws_security_group" "compliant" {
+  name        = "compliant-sg"
+  description = "Compliant SG"
+  vpc_id      = aws_vpc.custom.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "db" {
+  security_group_id            = aws_security_group.compliant.id
+  referenced_security_group_id = aws_security_group.source.id
+  from_port                    = 5432
+  to_port                      = 5432
+  ip_protocol                  = "tcp"
+}
+
+resource "aws_security_group" "non_compliant" {
+  name        = "non-compliant-sg"
+  description = "Non-Compliant SG"
+  vpc_id      = aws_vpc.custom.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https" {
+  security_group_id = aws_security_group.non_compliant.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  to_port           = 443
+  ip_protocol       = "tcp"
+}
--- rds-modules/rds.tf
+++ rds-modules/rds.tf
@@ -1,7 +1,9 @@
 module "database" {
-  source             = "./modules/rds"
-  project_name       = "rds-modules"
-  security_group_ids = []
+  source       = "./modules/rds"
+  project_name = "rds-modules"
+  security_group_ids = [
+    aws_security_group.compliant.id
+  ]
   subnet_ids = [
     aws_subnet.private1.id,
     aws_subnet.private2.id
